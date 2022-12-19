d237979d6ba32433649a9e571ec5ce49be7f2af4
ChiaLingHsu <h200022923@gmail.com>
2022-12-19 09:59:47 +0800

    上傳

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,27 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#E5B2FA',
+          secondary: '#B2EBF2',
+          accent: '#3F51B5'
+        }
+      }
+    }
+  }
+})
